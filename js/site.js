d1569195ec0de04d78d748eff0149bbf5388ae20
Fjeldhammer <fjeld.uilleam@gmail.com>
2023-07-26 09:19:36 -0600

    added JS code

--- js/site.js
+++ js/site.js
@@ -1,14 +1,66 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
+function getValues() {
 
-    let message = inputBox.value;
+    let userString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+    let revString = checkForPalindrome(userString);
+
+    let answers = displayResults(revString)
+
+    displayResults(answers);
+}
+
+function checkForPalindrome(input) {
+
+    input = input.replace(/[^a-zA-Z0-9]/g, '');
+
+    let revString = '';
+
+    for (let index = input.length - 1; index >= 0; index = index - 1) {
+
+        let letter = input[index];
+
+        revString = revString + letter;
+    }
+
+    let answers = []
+
+    if (revString == input) {
+        
+        answers.push(true)
+        answers.push(revString)
+
+    } else {
+
+        answers.push(false)
+        answers.push(revString)
+
+    }
+
+    return answers;
+}
+
+function displayResults(answers) {
+
+    let isPal = answers[0]
+    let reStr = answers[1]
+
+    if (isPal == true) {
+
+        document.getElementById('resultsY').textContent = reStr;
+
+        let alertBox = document.getElementById('alertYes');
+
+         alertBox.classList.remove('invisible');
+
+    } else {
+
+        document.getElementById('resultsN').textContent = reStr;
+
+        let alertBox = document.getElementById('alertNo');
+
+         alertBox.classList.remove('invisible');
+
+    }
+
 }
 
